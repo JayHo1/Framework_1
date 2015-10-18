84135d34a1a91b85d5933c59a6d635c84cc82165
Chau Nguyen HO <JayHo@iMac-de-Chau.local>
2015-10-19 00:42:00 +0200

    update

--- public/angularjs/modules/app.js
+++ public/angularjs/modules/app.js
@@ -1,1 +1,26 @@
-var app = angular.module('frame', []);
+
+var app = angular.module('myApp', ['pascalprecht.translate']);
+
+
+app.config(function ($translateProvider) {
+  $translateProvider.translations('en', {
+    TITLE: 'Sign In',
+    BIO: 'Biography',
+    BUTTON_LANG_EN: 'English',
+    BUTTON_LANG_FR: 'French'
+  });
+  $translateProvider.translations('fr', {
+    TITLE: 'Se Connecter',
+    BIO: 'Biographie',
+    BUTTON_LANG_EN: 'Anglais',
+    BUTTON_LANG_FR: 'Français'
+  });
+  	$translateProvider.preferredLanguage('en');
+});
+
+app.controller('Ctrl', function ($scope, $translate) {
+
+  $scope.changeLanguage = function (key) {
+    $translate.use(key);
+  }
+});
